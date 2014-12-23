b297ff9cea13e260acc8ceafb641666a1658c1ec
David Disseldorp <ddiss@suse.de>
2014-12-23 17:52:31 +0100

    lu: return error on invalid PERSISTENT_RESERVE_IN/OUT
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- src/istgt_lu_disk.c
+++ src/istgt_lu_disk.c
@@ -6891,7 +6891,7 @@ istgt_lu_disk_execute(CONN_Ptr conn, ISTGT_LU_CMD_Ptr lu_cmd)
 			if (lu_cmd->R_bit == 0) {
 				ISTGT_ERRLOG("R_bit == 0\n");
 				lu_cmd->status = ISTGT_SCSI_STATUS_CHECK_CONDITION;
-				return -1;
+				break;
 			}
 
 			sa = BGET8W(&cdb[1], 4, 5);
@@ -6900,7 +6900,7 @@ istgt_lu_disk_execute(CONN_Ptr conn, ISTGT_LU_CMD_Ptr lu_cmd)
 				ISTGT_ERRLOG("data_alloc_len(%d) too small\n",
 				    data_alloc_len);
 				lu_cmd->status = ISTGT_SCSI_STATUS_CHECK_CONDITION;
-				return -1;
+				break;
 			}
 			memset(data, 0, allocation_len);
 
@@ -6924,7 +6924,7 @@ istgt_lu_disk_execute(CONN_Ptr conn, ISTGT_LU_CMD_Ptr lu_cmd)
 			if (lu_cmd->W_bit == 0) {
 				ISTGT_ERRLOG("W_bit == 0\n");
 				lu_cmd->status = ISTGT_SCSI_STATUS_CHECK_CONDITION;
-				return -1;
+				break;
 			}
 
 			sa = BGET8W(&cdb[1], 4, 5);
